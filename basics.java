43f1cd26242657cd2e5a34197fa9e83a807a2d57
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-09 17:12:54 -0600

    While loop

--- basics.java
+++ basics.java
@@ -2,40 +2,9 @@ import java.util.Scanner;
 
 class fruits {
     public static void main(String[] args) {
-        int day = 0;
-        Scanner input1 = new Scanner(System.in);
-
-        do {
-            System.out.println("Enter the number day (1-7) 0 to exit: ");
-            day = input1.nextInt();
-            switch (day) {
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-                case 7:
-                    System.out.println("Sunday");
-                    break;
-                default:
-                    System.out.println("Enter a valid number");
-                    break;
-            }
-
-        } while (day != 0);
-
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i++);
+        }
     }
 }

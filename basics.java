d369e20d26422829062518097816423e242ac3d2
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-09 13:27:57 -0600

    Variables 1

--- basics.java
+++ basics.java
@@ -1,5 +1,14 @@
+import java.util.function.DoublePredicate;
+
 class fruits {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String name = "Ulises Fonseca Maya";
+        double quiz = 3.3;
+        boolean q = true;
+
+        System.out.println("Hello world " + name);
+        System.out.println(quiz);
+        System.out.println(q);
+
     }
 }

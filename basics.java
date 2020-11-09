95416ad2dc0b6bf080cdce1d1c1f576396a8524a
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-09 17:11:18 -0600

    Switch

--- basics.java
+++ basics.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 class fruits {
     public static void main(String[] args) {
-        int apples = 10;
-        System.out.println("Original apples: " + apples);
-        System.out.println("Post increment apples: " + (apples++));
-        System.out.println("Value apples: " + apples);
-        System.out.println("Pre increment apples: " + (++apples));
-        System.out.println("Value apples: " + apples);
+        int day = 0;
+        Scanner input1 = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter the number day (1-7) 0 to exit: ");
+            day = input1.nextInt();
+            switch (day) {
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("Enter a valid number");
+                    break;
+            }
+
+        } while (day != 0);
+
     }
 }

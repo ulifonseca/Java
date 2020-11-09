1e76e651806b6900e174f6156f20b45acb518bda
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-09 17:35:31 -0600

    Differet clases 2

---
+++ orange.java
@@ -0,0 +1,5 @@
+public class orange {
+    public void orange_info() {
+        System.out.println("Welcome to the oranges!");
+    }
+}
---
+++ tuna.java
@@ -0,0 +1,6 @@
+public class tuna {
+    public void simple_message() {
+        System.out.println("Hello Miry, I love you");
+    }
+    
+}

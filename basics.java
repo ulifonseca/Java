7e02fc7374aab578519bf24406659b107fe745e1
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-10 10:46:33 -0600

    Foreach 1

--- basics.java
+++ basics.java
@@ -2,17 +2,12 @@ import java.util.*;
 
 class fruits {
     public static void main(String[] args) {
-        int array1[] = new int[10];
-        int max = 10;
-        int min = 9;
-        Random r = new Random();
-        for (int i = 0; i < array1.length; i++) {
-            array1[i] = r.nextInt(max - min + 1) + min;
+        int array2[] = { 1, 2, 3, 4, 5 };
+        int sum =0;
+        for (int i : array2) {
+            sum+=i;
         }
-
-        for (int i = 0; i < array1.length; i++) {
-            System.out.println("Index " + i + " --> " + array1[i]);
-        }
+        System.out.println(x);
 
     }
 }

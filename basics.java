83dd0509ed5a37b6a07f34cbcee2ab21dfac6510
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-10 09:56:52 -0600

    Infinity grades average v2

--- basics.java
+++ basics.java
@@ -4,17 +4,18 @@ class fruits {
     public static void main(String[] args) {
         Scanner user1 = new Scanner(System.in);
         System.out.println("Grades of the semester calculator");
-        int grades = 0;
+        int grades = 1;
         double total = 0;
         double avg;
 
         do {
-            System.out.println("Enter grade " + (++grades) + " (0 to exit) :");
+            System.out.println("Enter grade " + (grades) + " (0 to exit) :");
             avg = user1.nextDouble();
-            total += avg;
             if (avg == 0)
                 grades--;
-
+            else
+                grades++;
+            total += avg;
         } while (avg != 0);
         System.out.println("Your average is: " + total / grades);
 

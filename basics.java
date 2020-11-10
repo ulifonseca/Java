75e0c64a9579fff015330395c2b002aad145a243
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-10 10:17:07 -0600

    Arrays 2 initialization

--- basics.java
+++ basics.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class fruits {
     public static void main(String[] args) {
-        int array1[] = new int[10];
-        for (int i = 0; i < array1.length; i++) {
-            array1[i] = i + 1;
-        }
+        int array1[] = { 1, 2, 3, 4, 5 };
+        /*
+         * for (int i = 0; i < array1.length; i++) { array1[i] = i * 2; }
+         */
         for (int i = 0; i < array1.length; i++) {
             System.out.println("Index " + i + " --> " + array1[i]);
         }

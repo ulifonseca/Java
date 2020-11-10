0c79143abe3b385b2995939fc15ce2dc9bf50252
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-10 09:42:49 -0600

    Grades average

--- basics.java
+++ basics.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class fruits {
     public static void main(String[] args) {
-        int age = 18;
-        System.out.println(age > 18 ? "You are legal" : age < 18 ? "You are illegal" : "You are 18");
+        Scanner user1 = new Scanner(System.in);
+        System.out.println("Grades of the semester calculator");
+        int grades;
+        double total = 0;
+        double avg;
+        System.out.print("Enter the number of subjects: ");
+        grades = user1.nextInt();
+
+        for (int i = 1; i <= grades; i++) {
+            System.out.println("Enter grade " + i + ": ");
+            avg = user1.nextDouble();
+            total += avg;
+        }
+        System.out.println("Your average is: " + total / grades);
+
     }
 }

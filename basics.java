4c5abe6bd15577d32dcab8806bf3264c42a2de9c
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-10 10:44:52 -0600

    Arrays 3 random filling

--- basics.java
+++ basics.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 class fruits {
     public static void main(String[] args) {
-        int array1[] = { 1, 2, 3, 4, 5 };
-        /*
-         * for (int i = 0; i < array1.length; i++) { array1[i] = i * 2; }
-         */
+        int array1[] = new int[10];
+        int max = 10;
+        int min = 9;
+        Random r = new Random();
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = r.nextInt(max - min + 1) + min;
+        }
+
         for (int i = 0; i < array1.length; i++) {
             System.out.println("Index " + i + " --> " + array1[i]);
         }

0f259d76efd8aa06d258abe65f9d3875276b6467
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-11 10:01:05 -0600

    Set time

--- basics.java
+++ basics.java
@@ -2,14 +2,8 @@ import java.util.*;
 
 class fruits {
     public static void main(String[] args) {
-        System.out.println(average(100, 90, 100));
-    }
-
-    public static double average(double... numbers) {
-        double result = 0;
-        for (double i : numbers) {
-            result += i;
-        }
-        return result / numbers.length;
+        tuna t1 = new tuna();
+        t1.set_time(23, 59, 0);
+        System.out.println(t1.to_military());
     }
 }
--- tuna.java
+++ tuna.java
@@ -1,20 +1,16 @@
 public class tuna {
-    private String name;
+    private int hour;
+    private int minute;
+    private int second;
 
-    public tuna(String n) {
-        name = n;
+    public void set_time(int h, int m, int s) {
+        hour = ((h >= 0 && h < 24) ? h : 0);
+        minute = ((m >= 0 && m < 60) ? m : 0);
+        second = ((s >= 0 && s < 60) ? s : 0);
     }
 
-    public void set_name(String n) {
-        name = n;
-    }
-
-    public String get_name() {
-        return name;
-    }
-
-    public void saying() {
-        System.out.println("Your sister is " + get_name());
+    public String to_military() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
     }
 
 }

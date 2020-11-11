0cf8cdb8776b40d0882ada05f29dad0cdef695ab
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-11 08:57:25 -0600

    Array, funtions different files

--- basics.java
+++ basics.java
@@ -2,46 +2,17 @@ import java.util.*;
 
 class fruits {
 
-    private static final int TOP_VALUE = 20;
-    public static final Random r = new Random();
-
-    public static void displayArray(int array[]) {
-        System.out.print("Array : [ ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.printf("%d ", array[i]);
-        }
-        System.out.println("]");
-    }
-
-    public static void fillArray(int array[]) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (i % TOP_VALUE);
-        }
-    }
-
-    public static void randomArray(int array[], int max, int min) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = r.nextInt(max - min + 1) + min;
-        }
-    }
-
-    public static void change(int array[]) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] *= 2;
-        }
-    }
-
     public static void main(String[] args) {
         int array[] = { 10, 10, 10, 30 };
-        displayArray(array);
-        fillArray(array);
-        displayArray(array);
+        orange.displayArray(array);
+        orange.fillArray(array);
+        orange.displayArray(array);
         int min = 10;
         int max = 15;
-        randomArray(array, max, min);
-        displayArray(array);
-        change(array);
-        displayArray(array);
+        orange.randomArray(array, max, min);
+        orange.displayArray(array);
+        orange.change(array);
+        orange.displayArray(array);
 
     }
 }
--- orange.java
+++ orange.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class orange {
+    private static final int TOP_VALUE = 20;
+    public static final Random r = new Random();
+
+    public static void displayArray(int array[]) {
+        System.out.print("Array : [ ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+        System.out.println("]");
+    }
+
+    public static void fillArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (i % TOP_VALUE);
+        }
+    }
+
+    public static void randomArray(int array[], int max, int min) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(max - min + 1) + min;
+        }
+    }
+
+    public static void change(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= 2;
+        }
+    }
 
 }

073ac6d96441abe82935c839cadef9293a0f0019
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-11 09:35:41 -0600

    Matrix 1

--- basics.java
+++ basics.java
@@ -1,26 +1,20 @@
 import java.util.*;
 
 class fruits {
-
-    public static void add(int array[], int add) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] += add;
+    public static void display_matrix(int a[][], String text) {
+        System.out.println(text);
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
         }
     }
-    public static void main(String[] args) {
-        int SIZE = 10;
-        int array[] = new int[SIZE];
-        orange.displayArray(array);
-        orange.fillArray(array);
-        orange.displayArray(array);
-        int min = 10;
-        int max = 15;
-        orange.randomArray(array, max, min);
-        orange.displayArray(array);
-        orange.change(array);
-        orange.displayArray(array);
-        add(array, 5);
-        orange.displayArray(array);
 
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][2];
+        int matrix2[][] = { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 }, { 4, 4, 4 } };
+        display_matrix(matrix, "Matrix 1...");
+        display_matrix(matrix2, "Matrix 2...");
     }
 }

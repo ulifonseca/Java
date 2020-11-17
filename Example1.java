939baf3e397ff07f8ebbfde42a81a33f01c61130
Ulises Fonseca <ulisesfonseca35@gmail.com>
2020-11-17 09:48:40 -0600

    Update java

---
+++ Example1.java
@@ -0,0 +1,12 @@
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("Love you Miry");
+        Example1.sayHi("Uli");
+        sayHi("Ulises Fonseca ");
+    }
+
+    public static void sayHi(String name) {
+        System.out.println("Hello my lady " + name);
+    }
+
+}
--- basics.java
+++ basics.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 class fruits {
     public static void main(String[] args) {
-        tuna t1 = new tuna();
-        t1.set_time(23, 59, 0);
-        System.out.println(t1.to_military());
+        Example1.main(args);
+        
     }
 }
